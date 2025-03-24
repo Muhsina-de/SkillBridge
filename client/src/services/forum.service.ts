921064ef90423ab11b5e377c464f5c1a115c7812
KnifeDad <boopyshnoot@gmail.com>
2025-03-24 17:56:58 -0400

    fix: Update forum service to use correct API endpoints

--- client/src/services/forum.service.ts
+++ client/src/services/forum.service.ts
@@ -1,19 +1,19 @@
 import axiosInstance from '../utils/axios';
 
 export const getTopics = () =>
-  axiosInstance.get('/forum/topics');
+  axiosInstance.get('/api/forum/topics');
 
 export const getTopic = (id: number) =>
-  axiosInstance.get(`/forum/topics/${id}`);
+  axiosInstance.get(`/api/forum/topics/${id}`);
 
 export const createTopic = (data: { title: string; content: string; category: string }) =>
-  axiosInstance.post('/forum/topics', data);
+  axiosInstance.post('/api/forum/topics', data);
 
 export const updateTopic = (id: number, data: { title: string; content: string; category: string }) =>
-  axiosInstance.put(`/forum/topics/${id}`, data);
+  axiosInstance.put(`/api/forum/topics/${id}`, data);
 
 export const deleteTopic = (id: number) =>
-  axiosInstance.delete(`/forum/topics/${id}`);
+  axiosInstance.delete(`/api/forum/topics/${id}`);
 
 export const createComment = (topicId: number, content: string) =>
-  axiosInstance.post(`/forum/topics/${topicId}/comments`, { content });
+  axiosInstance.post(`/api/forum/topics/${topicId}/comments`, { content });
